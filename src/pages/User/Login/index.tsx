fc1c40c368e5ffb97a8e1c19090bcf3687197a2e
cmd <3417372950@qq.com>
2024-07-08 23:18:21 +0800

    2024-07-08 23:17 生成图表

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -123,7 +123,7 @@ const Login: React.FC = () => {
         const urlParams = new URL(window.location.href).searchParams;
         history.push(urlParams.get('redirect') || '/');
         return;
-      }else{
+      } else {
         message.error(res.message);
       }
       // 如果失败去设置用户错误信息
